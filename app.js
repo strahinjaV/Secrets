a5fba2da2429bdc40f30c03985733c878f000971
strahinjaPC <strajo98@gmail.com>
2023-03-16 21:26:26 -0400

    made the register and login page able to route to secrets page if credentials are in order

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
+const mongoose = require("mongoose")
 
 const app = express()
-const PORT = 3000
+const PORT = "3000"
+const MONGOOSE_PORT = "27017"
+const mongoose_con = "mongodb://localhost:" + MONGOOSE_PORT + "/userDB"
 
 app.use(express.static("public"))
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
+
+mongoose.connect(mongoose_con)
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+User = new mongoose.model("User", userSchema)
 
 app.get("/", (req,res) => {
     res.render("home")
@@ -21,6 +33,31 @@ app.get("/register", (req,res) => {
     res.render("register")
 })
 
+app.post("/register", (req,res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save().then(() => {
+        res.render("secrets")
+    }).catch((err) => {
+        console.log(err)
+    })
+})
+
+app.post("/login", (req,res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({email: username}).then((foundUser) => {
+        if (foundUser.password === password){
+            res.render("secrets")
+        }
+    }).catch((err) => {
+        console.log(err)
+    })
+})
 
 
 
